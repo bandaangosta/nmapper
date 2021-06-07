a0e037bf9eb686c1f90abc5197009b82e5f036e7
Jose Ortiz <jlortiz@uc.cl>
2021-06-07 00:05:46 -0400

    Bumps version (patch)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="nmapper",
-    version="0.1.0",
+    version="0.1.1",
     description="Scan and list local network hosts",
     long_description=readme,
     long_description_content_type="text/markdown",
